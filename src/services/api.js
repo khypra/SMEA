a791f0ac37ea29c91530fb49660e34ebe2d3ca31
Matheus Simões <matheus.santos@dcomp.ufs.br>
2020-03-18 11:40:03 -0300

    Api routes for Cirurgias-limpas, Profissionais-saude, Acompanhamentos

--- src/services/api.js
+++ src/services/api.js
@@ -2,38 +2,72 @@ import api_interna from "./api_interna";
 
 export default class api {
   // Autentica o usuário e retorna o token Bearer equivalente ao login
+  /** Autentication route
+   * @param {nomeUsuario}
+   * @param {senha}
+   */
   static autenticate(nomeUsuario, senha) {
     return api_interna.post("auth/login", { nomeUsuario, senha });
   }
 
-  // Retorna os dados de um usuário e quais perfis ele possui nos demais sistemas
-  static getMe() {
-    return api_interna.get("user-info");
-  }
-
-  // Retorna todos os pacientes
-  static getPacients() {
-    return api_interna.get("pacientes");
-  }
-
-  /** Retorna um paciente através do número de prontuario
-   * @param {cod_pront}
-   */
-  static getPacient(cod_pront) {
-    return api_interna.get(`users/${cod_pront}`);
-  }
-
-  /** Armazena um novo paciente
-   * @param {paciente}
-   */
-  static createPacient({ paciente }) {
-    return api_interna.post("users", {});
-  }
-
-  /** Atualiza um paciênte existente
+  /** Atualiza um paciente existente
    * @param {paciente}
    */
   static updatePacient({ paciente }) {
     return api_interna.put(`users/${paciente.cod_pront}`, {});
+  }
+
+  //Profissionais-Saude Routes
+
+  //get all Profissionais-saude
+  static getProfissionaisSaude() {
+    return api_interna.get(`profissionais-saude`);
+  }
+
+  //get one Profissional-saude
+  static getProfissionalSaude({ id }) {
+    return api_interna.get(`profissionais-saude/${id}`);
+  }
+
+  //Acompanhamentos Routes
+
+  //get all Profissionais-saude
+  static getAcompanhamentos() {
+    return api_interna.get(`acompanhamentos`);
+  }
+
+  //get one Profissional-saude
+  static getAcompanhamento({ id }) {
+    return api_interna.get(`profissionais-saude/${id}`);
+  }
+
+  /**Create Acompanhamento
+   * @param {acompanhamento}
+   */
+  static createAcompanhamento({ acompanhamento }) {
+    return api_interna.post(`acompanhamentos`, {
+      cirurgiaLimpaId: acompanhamento.cirurgiaLimpaId,
+      responsavelPreenchimentoId: acompanhamento.responsavelPreenchimentoId,
+      permanenciaPacimente: acompanhamento.permanenciaPacimente,
+      reinternacao: acompanhamento.reinternacao,
+      usoProtese: acompanhamento.usoProtese,
+      eventoAdverso: acompanhamento.eventoAdverso,
+      isc: acompanhamento.isc
+    });
+  }
+
+  /**Update Acompanhamento
+   * @param {acompanhamento}
+   */
+  static updateAcompanhamento({ acompanhamento }) {
+    return api_interna.put(`acompanhamentos/${acompanhamento.id}`, {
+      cirurgiaLimpaId: acompanhamento.cirurgiaLimpaId,
+      responsavelPreenchimentoId: acompanhamento.responsavelPreenchimentoId,
+      permanenciaPacimente: acompanhamento.permanenciaPacimente,
+      reinternacao: acompanhamento.reinternacao,
+      usoProtese: acompanhamento.usoProtese,
+      eventoAdverso: acompanhamento.eventoAdverso,
+      isc: acompanhamento.isc
+    });
   }
 }
