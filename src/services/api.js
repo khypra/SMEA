91b85545efd930493576ac66ddcfbd56f9bedf2e
Matheus <khypra007@gmail.com>
2020-03-18 22:14:58 -0300

    Auto stash before merge of "Dev" and "origin/Dev"

--- src/services/api.js
+++ src/services/api.js
@@ -16,17 +16,26 @@ export default class api {
   static updatePacient({ paciente }) {
     return api_interna.put(`users/${paciente.cod_pront}`, {});
   }
+  //Rotas de Pacientes
 
   //Profissionais-Saude Routes
 
   //get all Profissionais-saude
   static getProfissionaisSaude() {
     return api_interna.get(`profissionais-saude`);
+  // Retorna todos os pacientes
+  static getPacientes() {
+    return api_interna.get("/pacientes");
   }
 
   //get one Profissional-saude
   static getProfissionalSaude({ id }) {
     return api_interna.get(`profissionais-saude/${id}`);
+  /** Retorna um paciente por id
+   * @param {id}
+   */
+  static getPaciente(id) {
+    return api_interna.get(`pacientes/${id}`);
   }
 
   //Acompanhamentos Routes
@@ -43,7 +52,11 @@ export default class api {
 
   /**Create Acompanhamento
    * @param {acompanhamento}
-   */
+  
+  //Rotas de Cirurgias Limpas
+
+  /** Armazena uma nova cirurgia
+   * @param {cirurgiaLimpa}
   static createAcompanhamento({ acompanhamento }) {
     return api_interna.post(`acompanhamentos`, {
       cirurgiaLimpaId: acompanhamento.cirurgiaLimpaId,
@@ -54,10 +67,14 @@ export default class api {
       eventoAdverso: acompanhamento.eventoAdverso,
       isc: acompanhamento.isc
     });
+  static createCirurgiaLimpa({ cirurgiaLimpa }) {
+    return api_interna.post("cirurgias-limpas", { cirurgiaLimpa });
   }
 
   /**Update Acompanhamento
    * @param {acompanhamento}
+  /** Atualiza uma cirurgia limpa existente
+   * @param {cirurgiaLimpa}
    */
   static updateAcompanhamento({ acompanhamento }) {
     return api_interna.put(`acompanhamentos/${acompanhamento.id}`, {
@@ -68,6 +85,19 @@ export default class api {
       usoProtese: acompanhamento.usoProtese,
       eventoAdverso: acompanhamento.eventoAdverso,
       isc: acompanhamento.isc
+    });
+  static updateCirurgiaLimpa({ cirurgiaLimpa }) {
+    return api_interna.put(`cirurgias-limpas/${cirurgiaLimpa.id}`, {
+      pacienteId: cirurgiaLimpa.pacienteId,
+      medicoCirurgiaoId: cirurgiaLimpa.medicoCirurgiaoId,
+      medicoAnestesistaId: cirurgiaLimpa.medicoAnestesistaId,
+      dataHoraInicio: cirurgiaLimpa.dataHoraInicio,
+      dataHoraFim: cirurgiaLimpa.dataHoraFim,
+      descricao: cirurgiaLimpa.descricao,
+      novaCirurgia: cirurgiaLimpa.novaCirurgia,
+      temperaturaMinimaSala: cirurgiaLimpa.temperaturaMinimaSala,
+      temperaturaMaximaSala: cirurgiaLimpa.temperaturaMaximaSala,
+      cirurgiaLimpa: cirurgiaLimpa.cirurgiaLimpa
     });
   }
 }
